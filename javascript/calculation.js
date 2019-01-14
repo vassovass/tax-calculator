5352813bb92f3910cce38491aaee3f7bb3b6fe11
vassovass <vasso@vaseo.co.za>
2019-01-14 23:31:08 +0200

    CSS and index file are copy pasted from https://github.com/Idrees-Mo/Tax-Calculator
    JS file is followed step by step from 
    https://www.youtube.com/watch?v=mq081fut8v0  tutorial , with comments 
    from me to show logic

---
+++ javascript/calculation.js
@@ -0,0 +1,8 @@
+//step 1 - select all the inputs and store into the variable
+
+let inputs = document.querySelectorAll('input')
+console.log("list of all inputs? " +inputs)
+function cal_Tax() {
+
+  let incomeRanges = [0,18000,34000,57000,180000,Number.MAX_VALUE];
+}
